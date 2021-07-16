26e7f9df40cc2a2bbc6f204023292e3c36df8a44
Filip <Karguel1995@gmail.com>
2021-07-16 15:23:31 +0200

    change in example: from paragraph to list

--- src/components/Pages/StudyPages/Verbs/ToBe.js
+++ src/components/Pages/StudyPages/Verbs/ToBe.js
@@ -53,51 +53,57 @@ const verbList = [
 
 const ToBe = () => {
 
-    const {selectedVerbList, setSelectedVerbList} = useContext(VerbsContext)
+  const {selectedVerbList, setSelectedVerbList} = useContext(VerbsContext)
 
-    useEffect(() => {
-    setSelectedVerbList(verbList)
-}, )
+  useEffect(() => {
+  setSelectedVerbList(verbList)
+})
 
-    return ( 
-        <div className="page">
-            <h1>To be - Być</h1>
-            <VerbsContainer verbList={selectedVerbList} />
+  return ( 
+    <div className="page">
+      <h1>To be - Być</h1>
+      <VerbsContainer verbList={selectedVerbList} />
+      <div className="verbExamples">
+        <h2 className="heading">Examples:</h2>
+        <h3>Present tense</h3>
+        <ul>
+          <li>Jestem wysoki - I am tall</li>
+          <li>Czy jesteś w domu? - Are you at home?</li>
+          <li>Jesteśmy w szkole - We are at school</li>
+          <li>Jestem z Polski - I am from Poland</li>
+          <li>Oni są w Hiszpanii - They are in Spain</li>
+          <li>Chcę być silny! - I want to be strong!</li>
+          <li>Gdzie jesteś? - Where are you?</li>
+          <li>Jestem tutaj - I am here</li>
+          <li>Co to jest? - What is that?</li>
+          <li>Kim ona jest? - Who is she?</li>
+          <li>Owoce są dobre! - Fruits are good!</li>
+          <li>Moi rodzice już tu są - My parents are already here</li>
+          <li>Jesteśmy szczęśliwą parą - We are a happy couple</li>
+          <li>Jesteście najlepsi - You are the best</li>
+          <li>Jesteście jeszcze w sklepie? - Are you still in the shop?</li>
+          <li>Twój brat jest z nami - Your brother is with us</li>
+          <li>Proszę, bądź miły - Please, be kind</li>
+          <li>Bądź w domu przed 22 - Be at home before 22</li>
+          <li>Nie bądź głupi - Don't be stupid</li>
+          <li>Bądźmy szczerzy - Let's (we) be honest</li>
+        </ul>
 
-<h2 className="heading">Examples:</h2>
-<h3>Present tense</h3>
-<p className="text">1. Jestem wysoki - I am tall</p>
-<p className="text">2. Czy jesteś w domu? - Are you at home?</p>
-<p className="text">3. Jesteśmy w szkole - We are at school</p>
-<p className="text">4. Jestem z Polski - I am from Poland</p>
-<p className="text">5. Oni są w Hiszpanii - They are in Spain</p>
-<p className="text">6. Chcę być silny! - I want to be strong!</p>
-<p className="text">7. Gdzie jesteś? - Where are you?</p>
-<p className="text">8. Jestem tutaj - I am here</p>
-<p className="text">9. Co to jest? - What is that?</p>
-<p className="text">10. Kim ona jest? - Who is she?</p>
-<p className="text">11. Owoce są dobre! - Fruits are good!</p>
-<p className="text">12. Moi rodzice już tu są - My parents are already here</p>
-<p className="text">13. Jesteśmy szczęśliwą parą - We are a happy couple</p>
-<p className="text">14. Jesteście najlepsi - You are the best</p>
-<p className="text">15. Jesteście jeszcze w sklepie? - Are you still in the shop?</p>
-<p className="text">16. Twój brat jest z nami - Your brother is with us</p>
+        <h3>Future tense</h3>
+        <ul>
+          <li>Zaraz będę w domu - I will be home soon</li>
+          <li>Dzisiaj będę się uczyć dwie godziny - Today I will study for two hours</li>
+          <li>O której będziesz w domu? - What time will you be home?</li>
+          <li>Co będziesz pić? - What will you drink? </li>
+          <li>To będzie najlepszy rok! - This will be the best year!</li>
+          <li>Tego lata Polska będzie mistrzem Świata - This summer, Poland will be the world champion</li>
+          <li>Czy będziecie w domu o 13? - Will you (plural) be home by 1pm? </li>
+          <li>Oni będą grać na komputerze dziś wieczorem - They will be playing computer games tonight</li>
+        </ul>
 
-<p className="text">17. Proszę, bądź miły - Please, be kind</p>
-<p className="text">18. Bądź w domu przed 22 - Be at home before 22</p>
-<p className="text">19. Nie bądź głupi - Don't be stupid</p>
-<p className="text">20. Bądźmy szczerzy - Let's be honest</p>
-<h3>Future tense</h3>
-<p className="text">1. Zaraz będę w domu - I will be home soon</p>
-<p className="text">2. Dzisiaj będę się uczyć dwie godziny - Today I will study for two hours</p>
-<p className="text">3. O której będziesz w domu? - What time will you be home?</p>
-<p className="text">4. Co będziesz pić? - What will you drink? </p>
-<p className="text">5. To będzie najlepszy rok! - This will be the best year!</p>
-<p className="text">6. Tego lata Polska będzie mistrzem Świata - This summer, Poland will be the world champion</p>
-<p className="text"></p>
-
-        </div>
-     );
+      </div>
+    </div>
+  );
 }
  
 export default ToBe;
